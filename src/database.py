78818340279d8cd5acbf3fa0eaecd00dbfa7d0b5
ignavan39 <ivanignatenko28@gmail.com>
2026-03-02 17:13:15 +0600

    feat: models & alembic

--- src/database.py
+++ src/database.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
+from sqlalchemy import Integer, func
 from sqlalchemy.ext.asyncio import AsyncAttrs, AsyncSession, create_async_engine
 from sqlalchemy.ext.asyncio import async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from configs.config import settings
 
 database_url = settings.get_db_config().get_url()
@@ -11,3 +14,7 @@ async_session = async_sessionmaker(engine,class_=AsyncSession, expire_on_commit=
 
 class Base(AsyncAttrs, DeclarativeBase):
     __abstract__ = True
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
+    deleted_at: Mapped[datetime | None]
---
+++ src/models/diary.py
@@ -0,0 +1,18 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from database import Base
+from datetime import date as date_type
+
+from models.user import User
+
+
+class Diary(Base):
+	__tablename__ = "diaries"
+	date: Mapped[date_type] = mapped_column(primary_key=True)
+	user_id: Mapped[int] = mapped_column(ForeignKey('users.id'),primary_key=True)
+	rating: Mapped[int]
+	user: Mapped["User"] = relationship(
+    "User",
+    back_populates="diaries"
+  )
