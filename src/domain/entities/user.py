2ad30cba2a75712628254e71822898ec0eaa4d67
ignavan39 <ivanignatenko28@gmail.com>
2026-03-05 04:54:08 +0600

    feat: change user_handler to funcs

--- src/domain/entities/user.py
+++ src/domain/entities/user.py
@@ -4,8 +4,9 @@ from typing import List, Optional
 from domain.entities.diary import Diary
 
 
-@dataclass(kw_only=True)
+@dataclass
 class User:
     user_id: int
+    id: Optional[int] = None
     name: Optional[str] = None
     diaries: List[Diary] = field(default_factory=list)
--- src/infrastructure/database/data_mappers/__init__.py
+++
@@ -1,2 +0,0 @@
-from .diary import diary_model_to_entity
-from .user import user_model_to_entity
