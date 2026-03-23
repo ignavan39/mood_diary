0b2c2d29de4be87c3434b839db12369950f91663
ignavan39 <ivanignatenko28@gmail.com>
2026-03-23 13:07:49 +0600

    feat: some refactoring

--- src/presintation/telegram/endpoints/mood/controllers/mood_infographic_controller.py
+++
@@ -1,8 +0,0 @@
-from application.use_cases.generate_mood_infographic import (
-    GenerateMoodInfographicUseCase,
-)
-
-
-class MoodInfographicController:
-    def __init__(self, use_case: GenerateMoodInfographicUseCase) -> None:
-        pass
