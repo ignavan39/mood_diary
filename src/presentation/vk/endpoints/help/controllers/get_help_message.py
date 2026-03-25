98fe7231cb855a6263ba68ba6acd9fa0b96f71c2
ignavan3 <ivanignatenko28@gmail.com>
2026-03-25 16:26:50 +0600

    feat: remove vkbottle bc loops conflitcs

--- src/presentation/vk/endpoints/help/controllers/get_help_message.py
+++
@@ -1,9 +0,0 @@
-from vkbottle.bot import Message
-
-from presentation.common.messages import Messages
-from presentation.vk.keyboards.main_menu import kb_main
-
-
-class GetHelpController:
-    async def call(self, message: Message) -> None:
-        await message.answer(Messages.HELP_TEXT, keyboard=kb_main())
